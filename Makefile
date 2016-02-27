08d7a2c419f757da7ce2d3c898177ed2f139459d
Sean Francis N. Ballais <sfballais123@gmail.com>
2016-02-27 20:36:27 +0800

    Added detection code.

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all:
-	g++ src/main.cpp -lopencv_core -lopencv_flann -lopencv_highgui -o bin/sudoko-grab
+	mkdir -p bin
+	g++ src/main.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -o bin/sudoko-grab
+	cp src/img/sudoku.jpg bin/
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,58 @@
-#include <opencv2/opencv.hpp>
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 int main()
 {
+    const int IMG_GRAYSCALE = 0;
+    cv::Mat sudoku = cv::imread("bin/sudoku.jpg", IMG_GRAYSCALE);
+    cv::Mat outerBox = cv::Mat(sudoku.size(), CV_8UC1);
+
+    cv::GaussianBlur(sudoku, sudoku, cv::Size(11, 11), 0);
+    cv::adaptiveThreshold(sudoku, outerBox, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 5, 2);
+    cv::bitwise_not(outerBox, outerBox);
+
+    cv::Mat kernel = (cv::Mat_<uchar>(3, 3) << 0, 1, 0, 1, 1, 1, 0, 1, 0);
+    cv::dilate(outerBox, outerBox, kernel);
+
+    // Detect the largest blob which is the puzzle
+    int count = 0;
+    int max = -1;
+
+    cv::Point maxPt;
+
+    for (int y = 0; y < outerBox.size().height; y++) {
+        uchar *row = outerBox.ptr(y);
+        for (int x = 0; x < outerBox.size().width; x++) {
+            if (row[x] >= 128) { // Make sure we only fill the pixel that is white
+                int area = cv::floodFill(outerBox, cv::Point(x, y), CV_RGB(0, 0, 64));
+                if (area > max) {
+                    maxPt = cv::Point(x, y);
+                    max = area;
+                }
+            }
+        }
+    }
+
+    // We got the biggest blob. Let's make it white.
+    cv::floodFill(outerBox, maxPt, CV_RGB(255, 255, 255));
+
+    // Turn the other blobs black
+    for (int y = 0; y < outerBox.size().height; y++) {
+        uchar *row = outerBox.ptr(y);
+        for (int x = 0; x < outerBox.size().width; x++) {
+            if (row[x] == 64 && x != maxPt.x && y != maxPt.y) {
+                int area = cv::floodFill(outerBox, cv::Point(x, y), CV_RGB(0, 0, 0));
+            }
+        }
+    }
+
+    // Erode the "dilated" image
+    cv::erode(outerBox, outerBox, kernel);
+    cv::imshow("Sudoku Grab", outerBox);
+    cv::waitKey(0);
+
     return 0;
 }
